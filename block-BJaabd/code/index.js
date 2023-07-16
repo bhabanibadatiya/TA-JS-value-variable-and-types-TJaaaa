0c31603859782a1a2274bb03c98ac1f3db998af0
bhabanisankar1901@gmail.com <bhabanisankar1901@gmail.com>
2023-07-16 11:17:04 +0530

    done

--- block-BJaabd/code/index.js
+++ block-BJaabd/code/index.js
@@ -1,88 +1,67 @@
-// Condition
-// Write your code below each problems:
+/* 1. Do the following
 
-/*
-Write a program that asks the user his/her age and check for the following conditions :
-
-- `if` the age is between 12-55 then print the message "You can participate in the marathon".
-- `if` the age is between 4-11 then print the message " You are too young to participate in the marathon".
-- `if` the age is less than 4 then print the message " Hey Kiddo! Can You Walk ?"
-- `if` the age is greater than 55 then print the message " You are too old to participate in the marthon".
-👇
+1. Declare a variable `user` and take the name using `prompt`. Display the value stored in `user` using `alert`.
+2. Now copy the value of `user` into a new variable named `userName`. Log the value of both `user` and `userName` using `console.log`.
+3. Declare variable `age` and `isLearning` with value `22` and `true` in the same line. hint: declaring multiplemvariable in one line.
+4. Now change the value of `user` to `John` and log the value of both `user` and `userName`.
 */
-
+let user = prompt( `Enter your name ?`)
+alert(user)
+let userName =user
+console.log(userName ,user)
+let age ='22'
+let isLearning='true'
+console.log( age,isLearning)
+user ='john'
+console.log( user ,userName)
+/* 2.
+Use `prompt` to take two numbers from user and store it in variable `numA` and `numB`. Create a third variable called `sum`, using `+` operator add numA and numB , store it in varible `sum` and using alert display `sum` in browser.
+*/
+let numA = prompt('enter a number?')
+let numB = prompt(' enter another number?')
+let sum = +numA + +numB
+alert(sum)
 // Loops
-/*
-Given a positive integer `n`. Print the word (hello) in format of heeeello (letter 'e' must be repeated `n` times). Take input from prompt and print the result in alert.
+
+// 3. Complete the following code to make the output be 0 2 4 6 8 10?
+
+for (let j = 0; j <= 10; j =j+1){
+    if( j % 2 ==0 ){
+    console.log(j);
+}}
+
+// 4. Print the odd numbers from 9 to 1 (both inclusive) using a for loop.
+for ( let j=9; j >=1; j = j-1 ){
+    if( j % 2 !==0){
+        console.log(j)
+    }
+}
+
+// 5. Concat all the numbers from 5 to 0 (both inclusive) in descending order using a while loop. (543210)
+let j= '5'
+while( j>=0 ){
+console.log(j); 
+
+j=j-1
+}
+
+// 6. Concat all the even numbers from 10 to 0 (both inclusive) in descending order using a while loop. (1086420)
+let l= '10'
+
+
+
+
+// Comparisoin
+
+/* 7. Take two value using prompt and store them in variables `num1` and `num2`. Check whether they are equal or not.
+
+- `alert` true or false base on the entered value.
+- if the input value is anything like `true`, `null` or `undefined` alert saying `Enter a valid value`.
+
 
 Example:
-n = 1 => output: hello
-n = 7 => output: heeeeeeello
-👇
-*/
-// [Your code goes here]
 
-/*
-Program to calculate the sum of first n natural numbers(1,2,3...n are known as natural numbers). Prompt user to enter n (using `prompt`) then based on input provided calculate and show result in `alert`.
-👇
-*/
-// [Your code goes here]
-
-/* Switch Statement
-
- 🎖Using switch statement do the following
-
-Take a number value from user and alert the message if it matches the conditions.
-* [ ] ONE, if `number` is equal to 1.
-* [ ] TWO, if `number` is equal to 2.
-* [ ] THREE, if `number` is equal to 3.
-* [ ] FOUR, if `number` is equal to 4.
-* [ ] FIVE, if `number` is equal to 5.
-* [ ] SIX, if `number` is equal to 6.
-* [ ] SEVEN, if `number` is equal to 7.
-* [ ] EIGHT, if `number` is equal to 8.
-* [ ] NINE, if `number` is equal to 9.
-* [ ] PLEASE TRY AGAIN, if  is none of the above.
+21, 21; // true
+"hello", -21; // false
 
 */
-// [Your code goes here]
-
-/*
-🎖Using switch statement do the following
-
-Take the value of `marks` (0-100) from user using `prompt` and `alert` the message (Your Grade is AA) as giver below.
-* [ ] `AA` if `marks` is greater than 90.
-* [ ] `AB` if `marks` is greater than 80 and less than or equal to 90
-* [ ] `BB` if `marks` is greater than 70 and less than or equal to 80
-* [ ] `BC` if `marks` is greater than 60 and less than or equal to 70
-* [ ] `CC` if `marks` is greater than 50 and less than or equal to 60
-* [ ] `CD` if `marks` is greater than 40 and less than or equal to 50
-* [ ] `DD` if `marks` is greater than 30 and less than or equal to 40
-* [ ] `FF` if `marks` is less than or equal to 30
-*/
-
-// [Your code goes here]
-
-/*
- 🎖Write a JavaScript program that takes two `integers` from user (using prompt) and alerts the larger number.
-*/
-// [your code goes here]
-
-/*
-🎖Write a JavaScript conditional statement to find the sign (+, -) of product of three numbers. Take those three numbers from user using `prompt`. Display an alert box with the specified sign.
-*/
-// [Your code goes here]
-
-/* Calculator
- 🎖Make a simple calculator with these functions. Using prompt, type conversion, if else statement. Use prompt to take the input from user i.e two numbers and an operation (Add, Sub, Mul, Div).
-
-  ⛑ Rule
-    * [ ] While substracting and dividing keep in mind the number one should be greater then number two. If not show alert saying `Number Two is larger then Number one`.
-  ⚡️ Operations
-    * [ ] Add
-    * [ ] Sub
-    * [ ] Mul
-    * [ ] Div
-*/
-
-// [Your code goes here]
